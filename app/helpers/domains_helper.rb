aaaf5a580f82d489692f900f8ca7d2d6426abed8
Brandt Lareau <brandt.lareau@gmail.com>
2012-03-21 01:41:39 -0700

    adde a layout htat puts a color for the status codes

--- app/helpers/domains_helper.rb
+++ app/helpers/domains_helper.rb
@@ -2,4 +2,12 @@ module DomainsHelper
   def favicon_uri(domain_url)
     domain_url + "favicon.ico"
   end
+  
+  def http_code_to_color(http_code)
+    if http_code == 200
+     content_tag(:span, http_code, :style => "color: green;")
+    else
+      content_tag(:span, http_code, :style => "color: red;")
+    end
+  end
 end
