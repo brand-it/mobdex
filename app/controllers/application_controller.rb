78cea449ac13d8814a962f89410eb38fbad3fe7c
Brandt Lareau <brandt.lareau@gmail.com>
2012-04-04 20:06:20 -0700

    added new layouts for mobile devices

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user_session, :current_user
   
-  before_filter :prepare_for_mobile
+  # before_filter :prepare_for_mobile
   
   # Failed authorization should all ways come before the return. Makes sure that the test
   # and the views are redirected before they are returned false.
@@ -106,19 +106,19 @@ class ApplicationController < ActionController::Base
   
   private 
   
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
-  end
-  helper_method :mobile_device?
-
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
-  end
+  # def mobile_device?
+  #      if session[:mobile_param]
+  #        session[:mobile_param] == "1"
+  #      else
+  #        request.user_agent =~ /Mobile|webOS/
+  #      end
+  #    end
+  #    helper_method :mobile_device?
+  #   
+  #    def prepare_for_mobile
+  #      session[:mobile_param] = params[:mobile] if params[:mobile]
+  #      request.format = :mobile if mobile_device?
+  #    end
   
   def basic
     return 1
